aa0a923b091118e9ed3bb2582a305a66db9249db
jessepesse <58948883+jessepesse@users.noreply.github.com>
2021-01-07 20:14:08 +0000

    Add files via upload

---
+++ scrtip.js
@@ -0,0 +1,21 @@
+var span = document.getElementById('time');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  var n = d.getTimezoneOffset();
+  if (s < 10) {
+	s = "0" + s;
+  }
+  if (m < 10) {
+	m = "0" + m;
+  }
+  if (h < 10) {
+	h = "0" + h;
+  }
+  span.textContent = h + ":" + m + ":" + s;
+}
+
+setInterval(time, 1000);
